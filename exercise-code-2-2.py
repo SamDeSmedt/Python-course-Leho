14440c48243f9bd0da66cd78d19bcd68303de0d4
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-03-31 16:35:07 +0200

    Added extra explanations to the exercise file

--- exercise-code-2-2.py
+++ exercise-code-2-2.py
@@ -2,14 +2,16 @@
 seq = "MNKMDLVADVAEKTDLSKAKATEVIDAVFA"
 
 # Display hardcoded sequence
-print("The default sequence is: {}".format(seq))
+print("The default sequence is: {}\n with length: {}".format(seq, len(seq)))
 
 # Perform slicing operation on the sequence
-# Show the characters with offset 3 each time
-slice1 = seq[0:9:3]
+
+# The third number indicates the characters to skip
+# Also known as step
+# Example takes first, fourth and seventh character
+slice1 = seq[0:9:3] # output: MVV
 print("Slicing the sequence with code seq[0:9:3] result in: {}".format(slice1))
-# 
-slice2 = seq[16:0:-4]
+slice2 = seq[16:0:-4] #output: SKDD
 print("Slicing the sequence with code seq[16:0:-4] result in: {}".format(slice2))
-slice3 = seq[:25:-1]
+slice3 = seq[:25:-1] #output: AFVA
 print("Slicing the sequence with code seq[:25:-1] result in: {}".format(slice3))
