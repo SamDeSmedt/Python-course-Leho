6f1a4477b57531cd0188e6e7a2228f19df811cf5
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-04-05 19:31:18 +0200

    Introduced the 'None' input for the start variable

--- exercise-code-3-5a.py
+++ exercise-code-3-5a.py
@@ -2,16 +2,20 @@
 # Script to the largest number from a script
 
 list = [2, 28, 11, 7, 40, 15]
-
+'''
 nLarge = 0
 
 if nLarge == 0:
     print("Before: None\n" + "-"*20)
 else:
     print("Before: {}\n".format(nLarge) + "-"*20)
+'''
+nLarge = None
+
+print("Before: {}".format(nLarge))
 
 for itervar in list:
-    if itervar > nLarge:
+    if nLarge is None or itervar > nLarge:
         nLarge = itervar
         print("Loop:\t{}\t{}".format(itervar,nLarge))
     else:
