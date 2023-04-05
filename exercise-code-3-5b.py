476b1e928cd546384091f2a387ee25ad5a379aad
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-04-05 19:37:40 +0200

    Added comments to show alternative solutions

--- exercise-code-3-5b.py
+++ exercise-code-3-5b.py
@@ -3,7 +3,7 @@
 
 # Variables
 count = 0
-total_sum = 0
+total_sum = 0.0 # Add the dot to create a float variable
 number = 1
 
 # input
@@ -12,7 +12,7 @@ print("Enter integers to calculate sum and average.\nInput 0 to exit.\n")
 while number != 0:
     number = int(input())
     total_sum += number
-    count = count + 1
+    count = count + 1 # count += 1
 else:
     avg = (total_sum / (count-1))
     print("Calculating sum and average for the above numbers:\nsum = {}\naverage = {}".format(total_sum,avg))
