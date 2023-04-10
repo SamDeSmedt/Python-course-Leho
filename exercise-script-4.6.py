e04d8c6154e1392a3bba1cc74716f7c00ee00b22
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-04-10 12:51:05 +0200

    Code to check password validity

---
+++ exercise-script-4.6.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Script to check password validity
+
+import re
+
+while True:
+    passwd = input("EGiva a valid password: ")
+    if (6 <= len(passwd) <= 16)\
+        and (any(c.islower() for c in passwd)\
+        and any(c.isupper() for c in passwd)\
+        and any(c.isdigit() for c in passwd)\
+        and any(c in "$#@" for c in passwd)):
+
+        print("Valid password")
+        break
+    else:
+        print("NOT a valid password!")
