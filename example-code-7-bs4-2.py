4eaf9bdecc324cd47e34dbcbacf96717a29bd893
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-04-24 23:00:38 +0200

    Added extra comments

--- example-code-7-bs4-2.py
+++ example-code-7-bs4-2.py
@@ -12,24 +12,24 @@ soup = BeautifulSoup(content, 'html.parser')
 
 # Tag object 1: example <table>
 tag_table = soup.table
-print("\nSTRING VALUE table tag --> {}".format(tag_table))
-print("\nNAME TAG --> {}".format(tag_table.name))
-print("\nATTRIBUTES TAG --> {}".format(tag_table.attrs))
+print("\nSTRING VALUE table tag --> {}".format(tag_table)) # Retrieve all table information; HTML like
+print("\nNAME TAG --> {}".format(tag_table.name)) # Name tag will be "table" for this exercise
+print("\nATTRIBUTES TAG --> {}".format(tag_table.attrs)) # Styling applied on the tag
 # Tag object 2: example <td>
 tag_td = soup.td
-print("\nSTRING VALUE td tag --> {}".format(tag_td))
-print("\nNAME TAG --> {}".format(tag_td.name))
-print("\nATTRIBUTES TAG --> {}".format(tag_td.attrs))
+print("\nSTRING VALUE td tag --> {}".format(tag_td)) # Retrieve all td information
+print("\nNAME TAG --> {}".format(tag_td.name)) # Name tag is "td"
+print("\nATTRIBUTES TAG --> {}".format(tag_td.attrs))# Styling applied on the tag
 # Tag object 3: multi-valued attributes example <div class>
 tag_h1 = soup.h1
-print("\nSTRING VALUE h1 tag --> {}".format(tag_h1))
-print("\nNAME TAG --> {}".format(tag_h1.name))
-print("\nATTRIBUTES h1 tag --> {}".format(tag_h1.attrs))
+print("\nSTRING VALUE h1 tag --> {}".format(tag_h1)) # Retrieve all h1 information
+print("\nNAME TAG --> {}".format(tag_h1.name)) # Name tag is "h1
+print("\nATTRIBUTES h1 tag --> {}".format(tag_h1.attrs)) # Styling applied on the tag
 print("List of values of attribute 'style' in h1 tag --> {}"
     .format(tag_h1['style']))
 
 # NavigableString
-tag_title = soup.title
+tag_title = soup.title # Directly get the string that is between the title tag
 print("\nNavigableString --> {}".format(tag_title.string))
 
 # Navigate tag
