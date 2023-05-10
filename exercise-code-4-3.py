14c62fc5c9162f98a2393c9295a1b2ffd3052f28
Sam De Smedt <sam.de.smedt@student.howest.be>
2023-05-10 11:49:44 +0200

    Introduced the matplotlib module as example

--- exercise-code-4-3.py
+++ exercise-code-4-3.py
@@ -2,7 +2,9 @@
 
 # Script to count the amount per base type and calculate the frequency
 
-input = input("Enter DNA sequence: ")
+#input = input("Enter DNA sequence: ")
+# Hardcoded input
+input = "AAGGTCGATCGATGATATCTTTCTAATTCTTATTATCTCTAACACCAAAGGAGATGATGACGTAGCTA"
 bases = {}
 
 def basefreq(input):
@@ -21,6 +23,21 @@ def basefreq(input):
 
     # Calcute the frequency of each base 
     for k,v in bases.items():
-        bases.update({k:(v*1.0/count)})
+        bases.update({k:(v*100.0/count)})
     return bases
 print("Calculate base frequency: \n{}".format(basefreq(input)))
+################################################################################
+# Visualise output using matplotlib
+################################################################################
+import matplotlib.pyplot as plt
+# Defining the values for the barplot
+base_key = list(bases.keys())
+base_value = list(bases.values())
+
+fig = plt.figure(figsize= (10,5))
+# Creating the barplot
+plt.bar(base_key, base_value, color="maroon", width=0.4)
+plt.xlabel("Base")
+plt.ylabel("Percentage")
+plt.title("Base percentage")
+plt.show()
